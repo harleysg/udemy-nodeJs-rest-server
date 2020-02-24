804a7bd6672c95f273a85034f5d11cd8fcf5b554
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-02-23 22:16:58 -0500

    update: change config enviroment

--- server/config.js
+++ server/config.js
@@ -2,3 +2,23 @@
  * @port
  */
 process.env.PORT = process.env.PORT || 3000;
+
+/**
+ * @ENVIROMENT
+ */
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+
+// Varidate Conection
+// ------------------------- Mongoose CONFIG
+let mongooseCONNECT;
+let mongoosePORT;
+let mongooseDB = "cafe";
+if (process.env.NODE_ENV) {
+	mongooseCONNECT = "mongodb://localhost";
+	mongoosePORT = ":27017/";
+} else {
+	mongooseCONNECT = "mongodb+srv://hgarzon:OyDtH90DdseGuVnW";
+	mongoosePORT = "@cluster0-tmkax.mongodb.net/";
+}
+
+process.env.MONGOOSEDB = mongooseCONNECT + mongoosePORT + mongooseDB;
